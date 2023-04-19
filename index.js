99fd733ff680323cca8cf46d6ed97f901308279c
Stuhren <you@example.com>
2023-04-19 15:41:14 +0200

    Testing gitignore.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //setting up the connection to MongoDB
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "insert_your_database_url_here";
+const mongoDB = "CONNECTION_URL";
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
